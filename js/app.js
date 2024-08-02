f0a7799ea17b0f980a7a380b498f79fbbb2f18e5
Abraham Castro <cucharadepalo@users.noreply.github.com>
2024-08-02 16:25:10 +0200

    Versión casi definitiva

--- js/app.js
+++ js/app.js
@@ -47,52 +47,52 @@ function initTabs() {
 	})
 
 	function setActiveTab(index) {
-		tabBtns[activeIndex].setAttribute("aria-selected", "false")
+		tabBtns[activeIndex].setAttribute('aria-selected', 'false')
 		tabBtns[activeIndex].tabIndex = -1
-		tabBtns[index].setAttribute("aria-selected", "true")
+		tabBtns[index].setAttribute('aria-selected', 'true')
 		tabBtns[index].tabIndex = 0
-		tabBtns[index].focus();
+		tabBtns[index].focus()
 	
-		setActivePanel(index);
+		setActivePanel(index)
 		activeIndex = index
 	}
 
 	function setActivePanel(index) {
-		panels[activeIndex].setAttribute("hidden", "")
+		panels[activeIndex].setAttribute('hidden', '')
 		panels[activeIndex].tabIndex = -1
-		panels[index].removeAttribute("hidden")
+		panels[index].removeAttribute('hidden')
 		panels[index].tabIndex = 0
 	}
 
 	tabBtns.forEach(function (tab, index) {
 
-		tab.addEventListener("keydown", function (event) {
+		tab.addEventListener('keydown', function (event) {
 			const lastIndex = tabBtns.length - 1;
 
-			if (event.code === "ArrowLeft" || event.code === "ArrowUp") {
-				event.preventDefault();
+			if (event.code === 'ArrowLeft' || event.code === 'ArrowUp') {
+				event.preventDefault()
 
 				if (activeIndex === 0) {
-					setActiveTab(lastIndex);
+					setActiveTab(lastIndex)
 				} else {
-					setActiveTab(activeIndex - 1);
+					setActiveTab(activeIndex - 1)
 				}
-			} else if (event.code === "ArrowRight" || event.code === "ArrowDown") {
-				event.preventDefault();
+			} else if (event.code === 'ArrowRight' || event.code === 'ArrowDown') {
+				event.preventDefault()
 
 				if (activeIndex === lastIndex) {
-					setActiveTab(0);
+					setActiveTab(0)
 				} else {
-					setActiveTab(activeIndex + 1);
+					setActiveTab(activeIndex + 1)
 				}
-			} else if (event.code === "Home") {
-				event.preventDefault();
+			} else if (event.code === 'Home') {
+				event.preventDefault()
 
-				setActiveTab(0);
-			} else if (event.code === "End") {
-				event.preventDefault();
+				setActiveTab(0)
+			} else if (event.code === 'End') {
+				event.preventDefault()
 
-				setActiveTab(lastIndex);
+				setActiveTab(lastIndex)
 			}
 		})
 	})
@@ -119,22 +119,72 @@ function openDetails(screenWidth) {
 	}
 }
 
-function initMoreInfoBtns() {
+function initModals() {
 	const btns = document.querySelectorAll('.event-more')
+	const modal = document.querySelector('.info-modal')
+	
 	btns.forEach( btn => {
 		btn.addEventListener('click', event => {
 			const id = btn.dataset.target
-			const dialog = document.getElementById(id)
-			console.log({id, dialog})
-			dialog.show()
+			const tpl = document.getElementById(id)
+			fillModalContent(tpl)
+			openModal()
 		})
 	})
+
+	document.addEventListener('click', event => {
+		if (event.target.classList.contains('info-modal') || event.target.classList.contains('modal-wrapper')) {
+			closeModal();
+		}
+	})
+
+	document.addEventListener('keydown', event => {
+		if (modal.classList.contains('open') && event.code == 'Escape') {
+			closeModal()
+		}
+	})
+}
+
+function fillModalContent(template) {
+	const wrapper = document.querySelector('.modal-content')
+	const clone = template.content.cloneNode(true)
+	wrapper.appendChild(clone)
+}
+
+function openModal() {
+	const modal = document.getElementById('modal')
+	const main = document.querySelector('main')
+
+	modal.classList.remove('hidden')
+	modal.setAttribute('aria-hidden', 'false')
+	main.setAttribute('aria-hidden', 'true')
+
+	setTimeout( () => {
+		document.body.classList.add('overflow-hidden')
+		modal.classList.add('open')
+	}, 100)
+}
+
+function closeModal() {
+	const modal = document.getElementById('modal')
+	const modalContent = document.querySelector('.modal-content')
+	const main = document.querySelector('main')
+
+	modal.classList.remove('open')
+	modal.setAttribute('aria-hidden', 'true')
+	main.setAttribute('aria-hidden', 'false')
+
+	setTimeout( () => {
+		modal.classList.add('hidden')
+		document.body.classList.remove('overflow-hidden')
+		modalContent.innerHTML = ''
+	}, 500)
 }
 
 // openDetails(window.innerWidth)
 initTabs()
 smoothScrollers()
-initMoreInfoBtns()
+initModals()
 window.addEventListener('resize', debounce(function (e) {
 	openDetails(window.innerWidth)
 }, 250, false))
